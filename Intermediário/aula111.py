087202f9975fa471cb9ddb73ae6fc361ef1a508b
Yukipyth <beatrizngon@hotmail.com>
2025-11-08 17:32:54 -0300

    4

---
+++ Intermediário/aula111.py
@@ -0,0 +1,12 @@
+# map - para mapear dados
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+produtos = [
+    {'nome': '', 'preco': 10.00},
+    {'nome': '', 'preco': 10.00},
+    {'nome': '', 'preco': 10.00},
+    {'nome': '', 'preco': 10.00},
+    {'nome': '', 'preco': 10.00},
+]
